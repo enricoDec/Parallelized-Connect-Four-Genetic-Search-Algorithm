b50cb9305002668b5d8b9fef3602a9e9f97e0ed2
enricoDec <enricodechadarevian@gmail.com>
2023-05-07 14:52:52 +0200

    changes nuklear in favour of raylib as gui library

--- Makefile
+++ Makefile
@@ -1,26 +1,22 @@
-# Install
-BIN = app
+CC := cc
+CFLAGS := $(shell pkg-config --cflags raylib)
+LDFLAGS := $(shell pkg-config --libs raylib)
 
-# Flags
-CFLAGS += -std=c99 -pedantic -O2
+SRCS := main.c
+OBJS := $(SRCS:.c=.o)
+BIN_DIR := ./bin
+TARGET := $(BIN_DIR)/app
 
-SRC = main.c
-OBJ = $(SRC:.c=.o)
+.PHONY: all clean
 
-ifeq ($(OS),Windows_NT)
-BIN := $(BIN).exe
-LIBS = -lglfw3 -lopengl32 -lm -lGLU32 -lGLEW32
-else
-	UNAME_S := $(shell uname -s)
-	GLFW3 := $(shell pkg-config --libs glfw3)
-	ifeq ($(UNAME_S),Darwin)
-		LIBS := $(GLFW3) -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -lm -lGLEW -L/usr/local/lib
-	else
-		LIBS = $(GLFW3) -lGL -lm -lGLU -lGLEW
-	endif
-endif
+all: $(TARGET)
 
-$(BIN):
-	@mkdir -p bin
-	rm -f bin/$(BIN) $(OBJS)
-	$(CC) $(SRC) $(CFLAGS) -o bin/$(BIN) $(LIBS)
+$(TARGET): $(OBJS)
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(CFLAGS) $(OBJS) -o $@ $(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(RM) -r $(BIN_DIR) $(OBJS)
--- main.c
+++ main.c
@@ -1,101 +1,156 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+/*******************************************************************************************
+*
+*   raylib [core] example - loading thread
+*
+*   NOTE: This example requires linking with pthreads library on MinGW, 
+*   it can be accomplished passing -static parameter to compiler
+*
+*   Example originally created with raylib 2.5, last time updated with raylib 3.0
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*
+*   Copyright (c) 2014-2023 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_GLFW_GL3_IMPLEMENTATION
-#define NK_KEYSTATE_BASED_INPUT
-#include "nuklear.h"
-#include "nuklear_glfw_gl3.h"
+#include "raylib.h"
 
-#define WINDOW_WIDTH 1200
-#define WINDOW_HEIGHT 800
+// WARNING: This example does not build on Windows with MSVC compiler
+#include "pthread.h"                        // POSIX style threads management
 
-#define MAX_VERTEX_BUFFER 512 * 1024
-#define MAX_ELEMENT_BUFFER 128 * 1024
+#include <stdatomic.h>                      // C11 atomic data types
 
-static void error_callback(int e, const char *d)
-{
-    printf("Error %d: %s\n", e, d);
-}
+#include <time.h>                           // Required for: clock()
 
+// Using C11 atomics for synchronization
+// NOTE: A plain bool (or any plain data type for that matter) can't be used for inter-thread synchronization
+static atomic_bool dataLoaded = false; // Data Loaded completion indicator
+static void *LoadDataThread(void *arg);     // Loading data thread function declaration
+
+static atomic_int dataProgress = 0;                // Data progress accumulator
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
 int main(void)
 {
-    /* GLFW */
-    static GLFWwindow *win;
-    int width = 0, height = 0;
-    struct nk_context *ctx;
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    glfwSetErrorCallback(error_callback);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - loading thread");
 
-    if (!glfwInit())
+    pthread_t threadId;             // Loading data thread id
+
+    enum { STATE_WAITING, STATE_LOADING, STATE_FINISHED } state = STATE_WAITING;
+    int framesCounter = 0;
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        fprintf(stdout, "[GFLW] failed to init!\n");
-        exit(1);
+        // Update
+        //----------------------------------------------------------------------------------
+        switch (state)
+        {
+            case STATE_WAITING:
+            {
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    int error = pthread_create(&threadId, NULL, &LoadDataThread, NULL);
+                    if (error != 0) TraceLog(LOG_ERROR, "Error creating loading thread");
+                    else TraceLog(LOG_INFO, "Loading thread initialized successfully");
+
+                    state = STATE_LOADING;
+                }
+            } break;
+            case STATE_LOADING:
+            {
+                framesCounter++;
+                if (atomic_load_explicit(&dataLoaded, memory_order_relaxed))
+                {
+                    framesCounter = 0;
+                    int error = pthread_join(threadId, NULL);
+                    if (error != 0) TraceLog(LOG_ERROR, "Error joining loading thread");
+                    else TraceLog(LOG_INFO, "Loading thread terminated successfully");
+
+                    state = STATE_FINISHED;
+                }
+            } break;
+            case STATE_FINISHED:
+            {
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    // Reset everything to launch again
+                    atomic_store_explicit(&dataLoaded, false, memory_order_relaxed);
+                    atomic_store_explicit(&dataProgress, 0, memory_order_relaxed);
+                    state = STATE_WAITING;
+                }
+            } break;
+            default: break;
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            switch (state)
+            {
+                case STATE_WAITING: DrawText("PRESS ENTER to START LOADING DATA", 150, 170, 20, DARKGRAY); break;
+                case STATE_LOADING:
+                {
+                    DrawRectangle(150, 200, atomic_load_explicit(&dataProgress, memory_order_relaxed), 60, SKYBLUE);
+                    if ((framesCounter/15)%2) DrawText("LOADING DATA...", 240, 210, 40, DARKBLUE);
+
+                } break;
+                case STATE_FINISHED:
+                {
+                    DrawRectangle(150, 200, 500, 60, LIME);
+                    DrawText("DATA LOADED!", 250, 210, 40, GREEN);
+
+                } break;
+                default: break;
+            }
+
+            DrawRectangleLines(150, 200, 500, 60, DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Connect Four", NULL, NULL);
-    glfwMakeContextCurrent(win);
-    glfwGetWindowSize(win, &width, &height);
-
-    /* OpenGL */
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-    glewExperimental = 1;
-    if (glewInit() != GLEW_OK)
-    {
-        fprintf(stderr, "Failed to setup GLEW\n");
-        exit(1);
-    }
-
-    /* Nuklear GUI */
-    ctx = nk_glfw3_init(win, NK_GLFW3_INSTALL_CALLBACKS);
-    /* Load Fonts: if none of these are loaded a default font will be used  */
-    /* Load Cursor: if you uncomment cursor loading please hide the cursor */
-    {
-        struct nk_font_atlas *atlas;
-        nk_glfw3_font_stash_begin(&atlas);
-        struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "res/fonts/Robotos-Regular.ttf", 14, 0);
-        nk_glfw3_font_stash_end();
-        nk_style_set_font(ctx, &roboto->handle);
-    }
-
-    while (!glfwWindowShouldClose(win))
-    {
-        /* Input */
-        glfwPollEvents();
-        nk_glfw3_new_frame();
-
-        /* GUI */
-
-        // nk_end(ctx);
-
-        /* Rendering */
-        glfwGetWindowSize(win, &width, &height);
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
-        glfwSwapBuffers(win);
-    }
-
-    nk_glfw3_shutdown();
-    glfwTerminate();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+// Loading data thread function definition
+static void *LoadDataThread(void *arg)
+{
+    int timeCounter = 0;            // Time counted in ms
+    clock_t prevTime = clock();     // Previous time
+
+    // We simulate data loading with a time counter for 5 seconds
+    while (timeCounter < 5000)
+    {
+        clock_t currentTime = clock() - prevTime;
+        timeCounter = currentTime*1000/CLOCKS_PER_SEC;
+
+        // We accumulate time over a global variable to be used in
+        // main thread as a progress bar
+        atomic_store_explicit(&dataProgress, timeCounter/10, memory_order_relaxed);
+    }
+
+    // When data has finished loading, we set global variable
+    atomic_store_explicit(&dataLoaded, true, memory_order_relaxed);
+
+    return NULL;
 }
