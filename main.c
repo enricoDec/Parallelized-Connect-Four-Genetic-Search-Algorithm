9a8f5ab0e957a9b58ef305c3cda2f61b0486b0ad
enricoDec <enricodechadarevian@gmail.com>
2023-05-09 18:11:53 +0200

    adds welcome screen

--- main.c
+++ main.c
@@ -1,156 +1,89 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - loading thread
-*
-*   NOTE: This example requires linking with pthreads library on MinGW, 
-*   it can be accomplished passing -static parameter to compiler
-*
-*   Example originally created with raylib 2.5, last time updated with raylib 3.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*
-*   Copyright (c) 2014-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-// WARNING: This example does not build on Windows with MSVC compiler
-#include "pthread.h"                        // POSIX style threads management
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
-#include <stdatomic.h>                      // C11 atomic data types
+#define MAX_DROPDOWN_ITEMS 3
 
-#include <time.h>                           // Required for: clock()
-
-// Using C11 atomics for synchronization
-// NOTE: A plain bool (or any plain data type for that matter) can't be used for inter-thread synchronization
-static atomic_bool dataLoaded = false; // Data Loaded completion indicator
-static void *LoadDataThread(void *arg);     // Loading data thread function declaration
-
-static atomic_int dataProgress = 0;                // Data progress accumulator
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+    bool isDebug = true;
+    bool startGame = false;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - loading thread");
+    // Game options
+    int boardSize = 0;      // 0: 7x6, 1: 8x8, 2: 9x7, 3: 9x9
+    int startingPlayer = 0; // 0: Random, 1: User, 2: PC
 
-    pthread_t threadId;             // Loading data thread id
+    InitWindow(screenWidth, screenHeight, "Connect Four");
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
 
-    enum { STATE_WAITING, STATE_LOADING, STATE_FINISHED } state = STATE_WAITING;
-    int framesCounter = 0;
+    // Const text
+    const char *WelcomeLabel = "WELCOME TO CONNECT FOUR!";
+    const char *StartButtonText = "START";
+    // Settings
+    const char *SettingsTitleLabel = "Settings";
+    const char *SelectBoardSizeLabel = "Select Board Size:";
+    const char *DropdownBoardSizes = "7x6;8x8;9x7;9x9";
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    const char *SelectStartPlayerLabel = "Select Starting Player:";
+    const char *DropdownStartPlayers = "Random;User;PC";
+
+    // Define controls variables
+    bool isEditingBoardSizeDropdown = false;
+    int BoardSizeDropdownIsActive = 0;
+    bool isEditingStartPlayerDropdown = false;
+    int StartPlayerDropdownIsActive = 0;
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        switch (state)
-        {
-            case STATE_WAITING:
-            {
-                if (IsKeyPressed(KEY_ENTER))
-                {
-                    int error = pthread_create(&threadId, NULL, &LoadDataThread, NULL);
-                    if (error != 0) TraceLog(LOG_ERROR, "Error creating loading thread");
-                    else TraceLog(LOG_INFO, "Loading thread initialized successfully");
-
-                    state = STATE_LOADING;
-                }
-            } break;
-            case STATE_LOADING:
-            {
-                framesCounter++;
-                if (atomic_load_explicit(&dataLoaded, memory_order_relaxed))
-                {
-                    framesCounter = 0;
-                    int error = pthread_join(threadId, NULL);
-                    if (error != 0) TraceLog(LOG_ERROR, "Error joining loading thread");
-                    else TraceLog(LOG_INFO, "Loading thread terminated successfully");
-
-                    state = STATE_FINISHED;
-                }
-            } break;
-            case STATE_FINISHED:
-            {
-                if (IsKeyPressed(KEY_ENTER))
-                {
-                    // Reset everything to launch again
-                    atomic_store_explicit(&dataLoaded, false, memory_order_relaxed);
-                    atomic_store_explicit(&dataProgress, 0, memory_order_relaxed);
-                    state = STATE_WAITING;
-                }
-            } break;
-            default: break;
-        }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
+        Vector2 mainAnchor = {screenWidth / 2, screenHeight / 2};
         BeginDrawing();
+        if (isDebug)
+        {
+            DrawFPS(screenWidth - 80, 0);
+        }
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            ClearBackground(RAYWHITE);
-
-            switch (state)
+        if (startGame)
+        {
+            // display selected Sele
+        }
+        else
+        {
+            if (isEditingBoardSizeDropdown || isEditingStartPlayerDropdown)
             {
-                case STATE_WAITING: DrawText("PRESS ENTER to START LOADING DATA", 150, 170, 20, DARKGRAY); break;
-                case STATE_LOADING:
-                {
-                    DrawRectangle(150, 200, atomic_load_explicit(&dataProgress, memory_order_relaxed), 60, SKYBLUE);
-                    if ((framesCounter/15)%2) DrawText("LOADING DATA...", 240, 210, 40, DARKBLUE);
-
-                } break;
-                case STATE_FINISHED:
-                {
-                    DrawRectangle(150, 200, 500, 60, LIME);
-                    DrawText("DATA LOADED!", 250, 210, 40, GREEN);
-
-                } break;
-                default: break;
+                GuiLock();
+            }
+            GuiLabel((Rectangle){screenWidth * 0.07f, screenHeight * 0.08f, screenWidth * 0.24f, screenHeight * 0.05f}, WelcomeLabel);
+            GuiLabel((Rectangle){screenWidth * 0.52f, screenHeight * 0.08f, screenWidth * 0.18f, screenHeight * 0.05f}, SettingsTitleLabel);
+            GuiLabel((Rectangle){screenWidth * 0.52f, screenHeight * 0.2f, screenWidth * 0.18f, screenHeight * 0.05f}, SelectBoardSizeLabel);
+            GuiLabel((Rectangle){screenWidth * 0.52f, screenHeight * 0.58f, screenWidth * 0.18f, screenHeight * 0.05f}, SelectStartPlayerLabel);
+            if (GuiButton((Rectangle){screenWidth * 0.07f, screenHeight * 0.36f, screenWidth * 0.2f, screenHeight * 0.1f}, StartButtonText))
+            {
+                startGame = true;
             }
 
-            DrawRectangleLines(150, 200, 500, 60, DARKGRAY);
+            if (GuiDropdownBox((Rectangle){screenWidth * 0.52f, screenHeight * 0.24f, screenWidth * 0.18f, screenHeight * 0.07f},
+                               DropdownBoardSizes, &BoardSizeDropdownIsActive, isEditingBoardSizeDropdown))
+            {
+                isEditingBoardSizeDropdown = !isEditingBoardSizeDropdown;
+            }
+            if (GuiDropdownBox((Rectangle){screenWidth * 0.52f, screenHeight * 0.64f, screenWidth * 0.18f, screenHeight * 0.07f},
+                               DropdownStartPlayers, &StartPlayerDropdownIsActive, isEditingStartPlayerDropdown))
+            {
+                isEditingStartPlayerDropdown = !isEditingStartPlayerDropdown;
+            }
+            GuiUnlock();
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
-}
-
-// Loading data thread function definition
-static void *LoadDataThread(void *arg)
-{
-    int timeCounter = 0;            // Time counted in ms
-    clock_t prevTime = clock();     // Previous time
-
-    // We simulate data loading with a time counter for 5 seconds
-    while (timeCounter < 5000)
-    {
-        clock_t currentTime = clock() - prevTime;
-        timeCounter = currentTime*1000/CLOCKS_PER_SEC;
-
-        // We accumulate time over a global variable to be used in
-        // main thread as a progress bar
-        atomic_store_explicit(&dataProgress, timeCounter/10, memory_order_relaxed);
-    }
-
-    // When data has finished loading, we set global variable
-    atomic_store_explicit(&dataLoaded, true, memory_order_relaxed);
-
-    return NULL;
 }
