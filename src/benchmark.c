3399193cf99a58588093dbb5673ac7666d9e80a1
enricoDec <enricodechadarevian@gmail.com>
2023-08-13 17:43:41 +0200

    replaces clock_gettime() with gettimeofday()

--- src/benchmark.c
+++ src/benchmark.c
@@ -1,28 +1,28 @@
 #include "includes/benchmark.h"
 
 #include <stdio.h>
-#include <sys/time.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-struct timespec start, end;
+struct timeval start, end;
 
 void benchmark_start(void)
 {
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    gettimeofday(&start, NULL);
 }
 
 void benchmark_end(void)
 {
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    gettimeofday(&end, NULL);
 }
 
 void benchmark_print(void)
 {
     double time_taken;
     time_taken = (end.tv_sec - start.tv_sec) * 1000.0;
-    time_taken += (end.tv_nsec - start.tv_nsec) / 1e6;
+    time_taken += (end.tv_usec - start.tv_usec) / 1000.0;
     printf("Time taken: %fms\n", time_taken);
-    // wirte to file for plotting
+    // write to file for plotting
     FILE *fp;
     fp = fopen("benchmark.txt", "a");
     fprintf(fp, "%fms\n", time_taken);
